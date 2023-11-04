bad399f7b8f2caeadf6dc111edf6dc4ecb834212
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-11-05 02:03:49 +0530

    Adding employee DAO implementation for getALL()

---
+++ src/main/java/com/example/anirudh/Accessor/dao/Impl/EmployeeDAOImpl.java
@@ -0,0 +1,26 @@
+package com.example.anirudh.Accessor.dao.Impl;
+
+import com.example.anirudh.Accessor.dao.EmployeeDAO;
+import com.example.anirudh.model.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+
+    private final EntityManager entityManager;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        // Note - createQuery - String arg shows error during compileTime but no effect during runtime.
+        TypedQuery<Employee> getAllEmployeesQuery = entityManager.createQuery("from employee", Employee.class);
+        return getAllEmployeesQuery.getResultList();
+    }
+}
