175d660c1bb3df148c4d2ea4780dede8e301e734
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-11-05 01:53:50 +0530

    adding starting code from local

---
+++ src/main/java/com/example/anirudh/Accessor/dao/EmployeeDAO.java
@@ -0,0 +1,10 @@
+package com.example.anirudh.Accessor.dao;
+
+import com.example.anirudh.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> getAllEmployees();
+}
