8a76ca3b026ff4133e0c75c7e1f4ff92473bd854
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-11-22 23:10:39 +0530

    removing validate interface as not currently in use

--- src/main/java/com/example/anirudh/Validator/Validate.java
+++
@@ -1,9 +0,0 @@
-package com.example.anirudh.Validator;
-
-public interface Validate {
-    /**
-     * Validate function, can be implemented by specific validators.
-     * @return
-     */
-    boolean validate();
-}
